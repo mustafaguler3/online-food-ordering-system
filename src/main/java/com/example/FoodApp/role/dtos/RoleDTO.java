9c2d7363a9cae316367b3149b80e99c77527a4b9
mustafa.glr <mustafa.glr@hotmail.com>
2025-06-22 12:54:34 +0300

    role entity,dto & repo added

---
+++ src/main/java/com/example/FoodApp/role/dtos/RoleDTO.java
@@ -0,0 +1,13 @@
+package com.example.FoodApp.role.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RoleDTO {
+    private long id;
+    private String name;
+}
