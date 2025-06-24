9493fbbdc8ac67e42d3fef71b65b741d1e1e967f
mustafa.glr <mustafa.glr@hotmail.com>
2025-06-24 14:20:04 +0300

    add auth service & its implementation

---
+++ src/main/java/com/example/FoodApp/auth_users/services/AuthService.java
@@ -0,0 +1,10 @@
+package com.example.FoodApp.auth_users.services;
+
+import com.example.FoodApp.auth_users.dtos.LoginRequest;
+import com.example.FoodApp.auth_users.dtos.RegistrationRequest;
+import com.example.FoodApp.response.Response;
+
+public interface AuthService {
+    Response<?> register(RegistrationRequest registrationRequest);
+    Response<?> login(LoginRequest loginRequest);
+}
